af5dc9267ef7ec270f5587f0d0b0c2798d8a8348
Tai1013 <frank22199215@gmail.com>
2023-03-22 16:13:42 +0800

    fix: _plugin-vue_export-helper error

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,10 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 import dotenv from 'dotenv'
 
+// eslint-disable-next-line no-control-regex
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 dotenv.config({ path: `./.env.${process.env.NODE_ENV}` })
 
 // https://vitejs.dev/config/
@@ -22,7 +26,22 @@ export default defineConfig({
   base: process.env.VITE_APP_FIREBASE_BASE,
   build: {
     outDir: 'docs',
-    assetsDir: 'assets'
+    assetsDir: 'assets',
+    assetsInlineLimit: 4096,
+    rollupOptions: {
+      output: {
+        // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+        sanitizeFileName(name) {
+          const match = DRIVE_LETTER_REGEX.exec(name);
+          const driveLetter = match ? match[0] : '';
+          // substr 是被淘汰語法，因此要改 slice
+          return (
+            driveLetter +
+            name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+          );
+        }
+      }
+    }
   },
   server: {
     port: 8000,
