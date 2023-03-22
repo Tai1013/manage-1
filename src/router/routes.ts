76e88a9b2348120922546d2a96f21bbf9cbfdf99
Tai1013 <frank22199215@gmail.com>
2023-03-22 15:51:27 +0800

    fix: router path

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,11 +2,11 @@ import { RouteRecordRaw } from 'vue-router'
 import { $t } from '@/i18n'
 
 export const routes: RouteRecordRaw[] = [
-  { path: '/login', name: 'login', component: () => import('@/views/login/index.vue'), meta: { pageName: $t('登入') } },
+  { path: '/login', name: 'login', component: () => import('../views/login/index.vue'), meta: { pageName: $t('登入') } },
   {
-    path: '/', name: 'pages', component: () => import('@/views/layout/index.vue'), meta: { requiresAuth: true },
+    path: '/', name: 'pages', component: () => import('../views/layout/index.vue'), meta: { requiresAuth: true },
     children: [
-      { path: '/', name: 'home', component: () => import('@/views/home/index.vue'), meta: { pageName: $t('首頁') } }
+      { path: '/', name: 'home', component: () => import('../views/home/index.vue'), meta: { pageName: $t('首頁') } }
     ]
   },
   { path: '/:pathMatch(.*)*', redirect: '/' }
